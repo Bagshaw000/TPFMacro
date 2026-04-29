328d25cfb302ff5a377a86acc5443f62863f208e
Omieibi Bagshaw <omieibibagshaw007@gmail.com>
2026-04-30 10:01:35 +1200

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -1,4 +1,7 @@
 import asyncio
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from httpx import AsyncClient
 from arq import create_pool
 from arq.connections import RedisSettings
