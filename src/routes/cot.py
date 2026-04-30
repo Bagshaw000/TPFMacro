31e085681aeb2aa9fa71192e286014ec09e23827
Omieibi Bagshaw <omieibibagshaw007@gmail.com>
2026-04-30 18:51:34 +1200

    Update cot.py

--- src/routes/cot.py
+++ src/routes/cot.py
@@ -1,9 +1,11 @@
 # All Cot routes will show here
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from controller.cot import COTController
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-
-from controller.cot import COTController
-
 
 router = APIRouter(prefix="/v1/cot_ttf")
 cot_obj = COTController()
